968316faa1263bab3803a897719447ff4cb1cb0c
Edward Laird <elaird@gmail.com>
2017-10-26 11:58:05 +0200

    move test to main file

--- drive_umnio.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-import time, umnio
-
-for data in [999, 64, 999, 78, 999]:
-    umnio.write_setting(data)
-    time.sleep(1)
--- umnio.py
+++ umnio.py
@@ -1,6 +1,18 @@
-import os
+#!/usr/bin/env python
+
+import os, time
+
 
 def write_setting(data):
     umnio_script = "umnio_user_data.%d" % data
     os.system("cat umnio_user_data.template | sed s@PHASE_SETTING@%d@ > %s " % (data, umnio_script))
     os.system("uMNioTool.exe hcal-uhtr-38-12 -o controlhub-hcal-daq -s %s" % umnio_script)
+
+
+def test():
+    for data in [999, 64, 999, 78, 999]:
+        write_setting(data)
+        time.sleep(1)
+
+if __name__ == "__main__":
+    test()
