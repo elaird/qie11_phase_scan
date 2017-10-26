cb0155a8a6453ac567738f774e27b8bff24d9bd4
Edward Laird <elaird@gmail.com>
2017-10-26 11:07:44 +0200

    extract fec functions; use generated settings

--- HE_phase_scan.py
+++ HE_phase_scan.py
@@ -1,129 +1,20 @@
 #!/usr/bin/env python
 
-import pexpect
-from datetime import datetime
-from time import time,sleep
-from re import search, escape
+from time import sleep
 from os import system
+import qie11_phase_scan as qps
 
+from fec_jm import sendngFECcommands, logResponse
 
 ##############################################
 ############### SCAN SETTINGS ################
 ##############################################
 logfile_name = "phasescan_log.txt" # note, log file is APPENDED, not overwritten
-phase_array = [78, 92, 42, 70, 84, 34, 48, 76, 90, 40, 68, 82, 32, 46, 74, 88, 38, 66, 80, 30, 44, 72, 86, 36, 64]
 transition_code = 999 # wrtitten to uMNIO during phase changes
 seconds_per_phase = 300
 loop = 100 # number of loops. loop = -1 for permanent looping
 test_mode = 0 # if testmode == 1 then there are no actual writes to hardware
 
-
-################################################
-############### SERVER SETTINGS ################
-################################################
-host = "hcalngccm02"
-port = 64000
-
-def sendngFECcommands(cmds=['quit']):
-        # HARDCODE FOR HE PHASE SCAN
-        # host = "hcalngccm02"
-        # port = 64000
-        script = False
-        raw = False
-
-	# Arguments and variables
-	output = []
-	raw_output = ""
-	# if script: 
-	# 	print 'Using script mode'
-	# else: 
-	# 	print 'Not using script mode'
-
-	# print cmds
-
-	if host != False and port:		# Potential bug if "port=0" ... (host should be allowed to be None.)
-		## Parse commands:
-		if isinstance(cmds, str):
-			cmds = [cmds]
-		if not script:
-			if "quit" not in cmds:
-				cmds.append("quit")
-		else:
-			cmds = [c for c in cmds if c != "quit"]		# "quit" can't be in a ngFEC script.
-			cmds_str = ""
-			for c in cmds:
-				cmds_str += "{0}\n".format(c)
-			file_script = "ngfec_script"
-			with open(file_script, "w") as out:
-				out.write(cmds_str)
-		
-		# Prepare the ngfec arguments:
-		ngfec_cmd = 'ngFEC.exe -z -c -p {0}'.format(port)
-		if host != None:
-			ngfec_cmd += " -H {0}".format(host)
-		
-		# Send the ngfec commands:
-		p = pexpect.spawn(ngfec_cmd)
-
-		if not script:
-			for i, c in enumerate(cmds):
-				if 'wait' in c:
-					# waitTime = int(c.split()[-1])
-					# sleep(waitTime/1000.)
-					continue
-				p.sendline(c)
-				if c != "quit":
-					t0 = time()
-					p.expect("{0}\s?#((\s|E)[^\r^\n^#]*)".format(escape(c)))
-					t1 = time()
-#					print [p.match.group(0)]
-					output.append({
-						"cmd": c,
-						"result": p.match.group(1).strip().replace("'", ""),
-						"times": [t0, t1],
-						"raw": p.before+p.after
-					})
-					raw_output += p.before + p.after
-		else:
-			p.sendline("< {0}".format(file_script))
-			for i, c in enumerate(cmds):
-				# Deterimine how long to wait until the first result is expected:
-				if i == 0:
-					timeout = max([30, int(0.2*len(cmds))])
-#					print i, c, timeout
-				else:
-					timeout = 30		# pexpect default
-#					print i, c, timeout
-#				print i, c, timeout
-				
-				# Send commands:
-				t0 = time()
-				p.expect(["{0}\s?#((\s|E)[^\r^\n^#]*)".format(escape(c)),'bingo'], timeout=timeout)
-				t1 = time()
-#				print [p.match.group(0)]
-				output.append({
-					"cmd": c,
-					"result": p.match.group(1).strip().replace("'", ""),
-					"times": [t0, t1],
-					"raw": p.before+p.after
-				})
-				raw_output += p.before + p.after
-			p.sendline("quit")
-		p.expect(pexpect.EOF)
-		raw_output += p.before
-#		sleep(1)		# I need to make sure the ngccm process is killed.
-		p.close()
-#		print "closed"
-		if raw:
-			return raw_output
-		else:
-			return output
-
-def logResponse(responses,logfile):
-        for response in responses:
-                logline =  str(response["cmd"]) + " ===> " + str(response["result"]) + "  --  " + str(datetime.now())
-                print logline
-                logfile.write(logline + "\n")
 
 def setPhase(phase):
 	logfile = open(logfile_name,"a")
@@ -146,7 +37,7 @@ print "Generating uMNIO script for phase " + str(transition_code) + " {transitio
 system("cat umnio_user_data.template | sed s@PHASE_SETTING@%d@ > %s " % (transition_code, umnio_script))
 
 while (loop != 0):
-        for phase in phase_array:
+        for phase in qps.settings():
 
                 print "Writing phase " +  str(transition_code) + " {transition} to uMNIO."
                 system("uMNioTool.exe hcal-uhtr-38-12 -o controlhub-hcal-daq -s %s" % "umnio_script." + str(transition_code))
---
+++ fec_jm.py
@@ -0,0 +1,112 @@
+import pexpect
+from datetime import datetime
+from time import time,sleep
+from re import search, escape
+
+################################################
+############### SERVER SETTINGS ################
+################################################
+host = "hcalngccm02"
+port = 64000
+
+def sendngFECcommands(cmds=['quit']):
+        # HARDCODE FOR HE PHASE SCAN
+        # host = "hcalngccm02"
+        # port = 64000
+        script = False
+        raw = False
+
+	# Arguments and variables
+	output = []
+	raw_output = ""
+	# if script: 
+	# 	print 'Using script mode'
+	# else: 
+	# 	print 'Not using script mode'
+
+	# print cmds
+
+	if host != False and port:		# Potential bug if "port=0" ... (host should be allowed to be None.)
+		## Parse commands:
+		if isinstance(cmds, str):
+			cmds = [cmds]
+		if not script:
+			if "quit" not in cmds:
+				cmds.append("quit")
+		else:
+			cmds = [c for c in cmds if c != "quit"]		# "quit" can't be in a ngFEC script.
+			cmds_str = ""
+			for c in cmds:
+				cmds_str += "{0}\n".format(c)
+			file_script = "ngfec_script"
+			with open(file_script, "w") as out:
+				out.write(cmds_str)
+		
+		# Prepare the ngfec arguments:
+		ngfec_cmd = 'ngFEC.exe -z -c -p {0}'.format(port)
+		if host != None:
+			ngfec_cmd += " -H {0}".format(host)
+		
+		# Send the ngfec commands:
+		p = pexpect.spawn(ngfec_cmd)
+
+		if not script:
+			for i, c in enumerate(cmds):
+				if 'wait' in c:
+					# waitTime = int(c.split()[-1])
+					# sleep(waitTime/1000.)
+					continue
+				p.sendline(c)
+				if c != "quit":
+					t0 = time()
+					p.expect("{0}\s?#((\s|E)[^\r^\n^#]*)".format(escape(c)))
+					t1 = time()
+#					print [p.match.group(0)]
+					output.append({
+						"cmd": c,
+						"result": p.match.group(1).strip().replace("'", ""),
+						"times": [t0, t1],
+						"raw": p.before+p.after
+					})
+					raw_output += p.before + p.after
+		else:
+			p.sendline("< {0}".format(file_script))
+			for i, c in enumerate(cmds):
+				# Deterimine how long to wait until the first result is expected:
+				if i == 0:
+					timeout = max([30, int(0.2*len(cmds))])
+#					print i, c, timeout
+				else:
+					timeout = 30		# pexpect default
+#					print i, c, timeout
+#				print i, c, timeout
+				
+				# Send commands:
+				t0 = time()
+				p.expect(["{0}\s?#((\s|E)[^\r^\n^#]*)".format(escape(c)),'bingo'], timeout=timeout)
+				t1 = time()
+#				print [p.match.group(0)]
+				output.append({
+					"cmd": c,
+					"result": p.match.group(1).strip().replace("'", ""),
+					"times": [t0, t1],
+					"raw": p.before+p.after
+				})
+				raw_output += p.before + p.after
+			p.sendline("quit")
+		p.expect(pexpect.EOF)
+		raw_output += p.before
+#		sleep(1)		# I need to make sure the ngccm process is killed.
+		p.close()
+#		print "closed"
+		if raw:
+			return raw_output
+		else:
+			return output
+
+def logResponse(responses,logfile):
+        for response in responses:
+                logline =  str(response["cmd"]) + " ===> " + str(response["result"]) + "  --  " + str(datetime.now())
+                print logline
+                logfile.write(logline + "\n")
+
