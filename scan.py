147724f14135c9e24a4a590d869df59edc31edbc
Edward Laird <elaird@gmail.com>
2020-02-13 16:28:35 +0100

    expand support of HB/HE/HF

--- scan.py
+++ scan.py
@@ -5,25 +5,25 @@ import fec_jm, umnio
 import qie11_phases, tdc_thresholds
 
 
-def applySetting(module, setting, test_mode=None, logfile=None):
-    cmds1 = module.commands(setting, put=True)
-    cmds2 = module.commands(setting, put=False)
+def applySetting(module, setting, subdet, client, host, port, test_mode, logfile=None):
+    cmds  = module.commands(setting, subdet, put=True)
+    cmds += module.commands(setting, subdet, put=False)
 
     if test_mode:
-        logfile.write("Test mode enabled. The following commands would be sent to the ngccm server otherwise:\n")
-        for cmd in cmds1 + cmds2:
-            logfile.write(cmd + "\n")
+        fec_jm.onlyLog(client, host, port, cmds, logfile)
     else:
-        fec_jm.sendAndLog(cmds1, logfile)
-        fec_jm.sendAndLog(cmds2, logfile)
-    logfile.write("############################################\n")
+        fec_jm.sendAndLog(client, host, port, cmds, logfile)
 
 
 def parsed_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--code", help="code written to uMNio during change of setting", metavar="N", type=int, default=999)
     parser.add_argument("--cycles", help="number of cycles (negative will permanently loop)", type=int, default=20)
+    parser.add_argument("--hb", help="scan HB settings", action="store_true", default=False)
+    parser.add_argument("--he", help="scan HE settings", action="store_true", default=False)
+    parser.add_argument("--hf", help="scan HF settings", action="store_true", default=False)
     parser.add_argument("--logfile", help="log file to append to", default="scan_log.txt")
+    parser.add_argument("--ngfec-exe", help="use this program to communicate with the server", default="/nfshome0/hcalpro/ngFEC/ngFEC.exe")
     parser.add_argument("--seconds", help="number of seconds to sleep per setting", type=int, default=300)
     parser.add_argument("--tdc-threshold", help="Scan TDC threshold rather than phase", action='store_true', default=False)
     parser.add_argument("--test-mode", help="Don't interact with hardware", action='store_true', default=False)
@@ -41,7 +41,12 @@ def main(args):
                 umnio.write_setting(args.code)
 
             print("Applying setting: %d" % setting)
-            applySetting(module, setting, test_mode=args.test_mode, logfile=logfile)
+            if args.hb:
+                applySetting(module, setting, "HB", args.ngfec_exe, "hcalngccm03", 64400, args.test_mode, logfile=logfile)
+            if args.he:
+                applySetting(module, setting, "HE", args.ngfec_exe, "hcalngccm02", 64000, args.test_mode, logfile=logfile)
+            if args.hf:
+                applySetting(module, setting, "HF", args.ngfec_exe, "hcalngccm01", 63000, args.test_mode, logfile=logfile)
 
             print("Writing value %d to uMNIO." % setting)
             if not args.test_mode:
@@ -50,9 +55,9 @@ def main(args):
             print("...sleeping")
             time.sleep(args.seconds)
 
-            print("####################################################################################")
-
+            logfile.write("############################################\n")
             logfile.close()
+
         args.cycles -= 1
 
 
