e4f743dda00e2194f110aee3fc0ce717481ad369
Edward Laird <elaird@gmail.com>
2020-02-13 15:53:20 +0100

    support both HB and HE

--- qie11_phases.py
+++ qie11_phases.py
@@ -42,13 +42,27 @@ def settings(nCycles=1, debug=False):
     return out
 
 
-def commands(setting, rms="[1-4]", qies="[1-48]", put=False):
+def commands(setting, subdet, put=False):
     out = []
+
+    if subdet == "HB":
+        sectors = "[01-18]"
+        rms = "[1-4]"
+        qies = "[1-64]"
+        n_per_end = 18 * 4 * 64
+    elif subdet == "HE":
+        sectors = "[01-18]"
+        rms = "[1-4]"
+        qies = "[1-48]"
+        n_per_end = 18 * 4 * 48
+    else:
+        print("Subdetector %s is not supported." % subdet)
+        return out
+
     for end in "MP":
-        stem = "HE%s[01-18]-%s" % (end, rms)
+        stem = "%s%s%s-%s" % (subdet, end, sectors, rms)
         if put:
-            mult = 4 * 48 * 18
-            out.append("put %s-QIE%s_PhaseDelay %d*%d" % (stem, qies, mult, setting))
+            out.append("put %s-QIE%s_PhaseDelay %d*%d" % (stem, qies, n_per_end, setting))
         else:
             out.append("get %s-QIE%s_PhaseDelay_rr" % (stem, qies))
     return out
@@ -63,8 +77,8 @@ def test():
     assert sorted(t) == list(range(min(t), 1 + max(t)))
 
     for s in settings(debug=True):
-        print(commands(s, put=True))
-        print(commands(s, put=False))
+        print(commands(s, "HB", put=True))
+        print(commands(s, "HB", put=False))
 
     
 if __name__ == "__main__":
