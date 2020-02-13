ce4846f0af3ba5778ca805edc5b503efd048dbfe
Edward Laird <elaird@gmail.com>
2020-02-13 15:53:31 +0100

    support both HB and HE

--- tdc_thresholds.py
+++ tdc_thresholds.py
@@ -11,13 +11,27 @@ def settings(nCycles=1, debug=False):
     return out * nCycles
 
 
-def commands(setting, rms="[1-4]", qies="[1-64]", put=False):
+def commands(setting, subdet, put=False):
     out = []
+
+    if subdet == "HB":
+        sectors = "[01-18]"
+        rms = "[1-4]"
+        qies = "[1-64]"
+        n_per_end = 18 * 4 * 64
+    elif subdet == "HE":
+        sectors = "[01-18]"
+        rms = "[1-4]"
+        qies = "[1-48]"
+        n_per_end = 18 * 4 * 48
+    else:
+        print("Subdetector %s is not supported." % subdet)
+        return out
+
     for end in "MP":
-        stem = "HB%s[01-18]-%s" % (end, rms)
+        stem = "%s%s%s-%s" % (subdet, end, sectors, rms)
         if put:
-            mult = 4 * 64 * 18
-            out.append("put %s-QIE%s_TimingThresholdDAC %d*%d" % (stem, qies, mult, setting))
+            out.append("put %s-QIE%s_TimingThresholdDAC %d*%d" % (stem, qies, n_per_end, setting))
         else:
             out.append("get %s-QIE%s_TimingThresholdDAC" % (stem, qies))
     return out
