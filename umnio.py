2d138364152e3450e14ac4074fef6140948469e8
Edward Laird <elaird@gmail.com>
2020-02-19 18:31:58 +0100

    slim output somewhat

--- umnio.py
+++ umnio.py
@@ -7,7 +7,7 @@ def write_setting(data):
     template = "umnio_user_data.template"
     script = template.replace(".template", ".%d" % data)
     os.system("cat %s | sed s@THE_VALUE@%d@ > %s " % (template, data, script))
-    os.system("uMNioTool.exe hcal-uhtr-38-12 -o bridge-ho -s %s" % script)
+    os.system("uMNioTool.exe hcal-uhtr-38-12 -o bridge-ho -s %s | grep -A 9 set" % script)
 
 
 def test():
